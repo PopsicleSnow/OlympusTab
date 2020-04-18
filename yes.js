42c675a5196c05879187026a5c9cfd62460d5093
PopsicleSnow <57603239+PopsicleSnow@users.noreply.github.com>
2020-04-18 16:29:30 -0700

    settings button

--- yes.js
+++ yes.js
@@ -1,17 +1,30 @@
+web_engine = document.getElementById("web_engine")
+hover_button = document.getElementById("hover_button")
 document.getElementById("google").addEventListener("click", function(){engine_change("google");});
 document.getElementById("duckduckgo").addEventListener("click", function(){engine_change("duckduckgo");});
 document.getElementById("bing").addEventListener("click", function(){engine_change("bing");});
 function engine_change(engine){
     if (engine == "google"){
-        document.getElementById("web_engine").action = "https://www.google.com/search";
-        document.getElementById("hover_button").src = "images/google_icon.png";
+        web_engine.action = "https://www.google.com/search";
+        hover_button.src = "images/google_icon.png";
     }
     if (engine == "duckduckgo"){
-        document.getElementById("web_engine").action = "https://www.duckduckgo.com/";
-        document.getElementById("hover_button").src = "images/duckduckgo.png";
+        web_engine.action = "https://www.duckduckgo.com/";
+        hover_button.src = "images/duckduckgo.png";
     }
     if (engine == "bing"){
-        document.getElementById("web_engine").action = "https://www.bing.com/search";
-        document.getElementById("hover_button").src = "images/bing_icon.png";
+        web_engine.action = "https://www.bing.com/search";
+        hover_button.src = "images/bing_icon.png";
+    }
+}
+
+
+settings_screen = $(".settings_screen");
+document.getElementById("setting_button").onclick = function() {
+    if (settings_screen.css('display') == "none") {
+        settings_screen.css('display', 'block');           
+    }
+    else{
+        settings_screen.css('display', 'none');
     }
 }
