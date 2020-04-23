cd21b596f2b93d1ef38cd1a7d8167dbd61e566bd
PopsicleSnow <57603239+PopsicleSnow@users.noreply.github.com>
2020-04-22 21:28:02 -0700

    setup for settings

--- yes.js
+++ yes.js
@@ -1,20 +1,30 @@
-web_engine = document.getElementById("web_engine")
-hover_button = document.getElementById("hover_button")
-document.getElementById("google").addEventListener("click", function(){engine_change("google");});
-document.getElementById("duckduckgo").addEventListener("click", function(){engine_change("duckduckgo");});
-document.getElementById("bing").addEventListener("click", function(){engine_change("bing");});
+let web_engine = document.getElementById("web_engine");
+let hover_button = document.getElementById("hover_button");
+let google = document.getElementById("google");
+let duckduckgo = document.getElementById("duckduckgo");
+let bing = document.getElementById("bing");
+google.addEventListener("click", function(){engine_change("google");});
+duckduckgo.addEventListener("click", function(){engine_change("duckduckgo");});
+bing.addEventListener("click", function(){engine_change("bing");});
 function engine_change(engine){
+    google.style.display = '';
+    duckduckgo.style.display = '';
+    bing.style.display = '';
     if (engine == "google"){
         web_engine.action = "https://www.google.com/search";
         hover_button.src = "images/google_icon.png";
+        google.style.display = 'None';
     }
     if (engine == "duckduckgo"){
         web_engine.action = "https://www.duckduckgo.com/";
         hover_button.src = "images/duckduckgo.png";
+        duckduckgo.style.display = 'None';
     }
     if (engine == "bing"){
         web_engine.action = "https://www.bing.com/search";
         hover_button.src = "images/bing_icon.png";
+        bing.style.display = 'None';
+        duckduckgo.className = ".last_one";
     }
 }
 
