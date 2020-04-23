f2db90f6e695338a4529d51d998c38fc52763c52
PopsicleSnow <57603239+PopsicleSnow@users.noreply.github.com>
2020-04-23 13:38:33 -0700

    clean up drop down options
    Remove a selected search engine from the drop down contents

--- yes.js
+++ yes.js
@@ -1,8 +1,9 @@
-let web_engine = document.getElementById("web_engine");
-let hover_button = document.getElementById("hover_button");
-let google = document.getElementById("google");
-let duckduckgo = document.getElementById("duckduckgo");
-let bing = document.getElementById("bing");
+var web_engine = document.getElementById("web_engine");
+var hover_button = document.getElementById("hover_button");
+var google = document.getElementById("google");
+var duckduckgo = document.getElementById("duckduckgo");
+var bing = document.getElementById("bing");
+google.style.display = "none";
 google.addEventListener("click", function(){engine_change("google");});
 duckduckgo.addEventListener("click", function(){engine_change("duckduckgo");});
 bing.addEventListener("click", function(){engine_change("bing");});
@@ -13,21 +14,26 @@ function engine_change(engine){
     if (engine == "google"){
         web_engine.action = "https://www.google.com/search";
         hover_button.src = "images/google_icon.png";
-        google.style.display = 'None';
+        google.style.display = "none";
     }
     if (engine == "duckduckgo"){
         web_engine.action = "https://www.duckduckgo.com/";
         hover_button.src = "images/duckduckgo.png";
-        duckduckgo.style.display = 'None';
+        duckduckgo.style.display = "none";
     }
     if (engine == "bing"){
         web_engine.action = "https://www.bing.com/search";
         hover_button.src = "images/bing_icon.png";
-        bing.style.display = 'None';
-        duckduckgo.className = ".last_one";
+        bing.style.display = "none";
     }
+    $(".dropdown-content button").css({
+        'border-bottom-left-radius': '0px',
+        'border-bottom-right-radius': '0px'});
+    let last_visible_dropdown = $('.dropdown-content button:visible:last');
+    last_visible_dropdown.css({
+        'border-bottom-left-radius': '20px',
+        'border-bottom-right-radius': '20px'});
 }
-
 
 settings_screen = $(".settings_screen");
 document.getElementById("setting_button").onclick = function() {
