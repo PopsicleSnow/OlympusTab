6ca6c4af0a3f4e224079bca523abbf56268949f9
PopsicleSnow <57603239+PopsicleSnow@users.noreply.github.com>
2020-06-26 13:45:05 -0700

    Implement basic chrome local storage

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ with Chrome extension support
 View it here: https://popsiclesnow.github.io/custom-new-tab-chrome/custom_tab.html.
 
 ![screenshot of page](tab.png)
+
+TODO
+ - Add settings option to change background color using chrome.storage.local.set
+ - Add custom search engine logo above search bar with toggle to remove it in settings
--- yes.js
+++ yes.js
@@ -3,28 +3,42 @@ var hover_button = document.getElementById("hover_button");
 var google = document.getElementById("google");
 var duckduckgo = document.getElementById("duckduckgo");
 var bing = document.getElementById("bing");
-google.style.display = "none";
+
+// Event listeners for click of buttons under dropdown
 google.addEventListener("click", function(){engine_change("google");});
 duckduckgo.addEventListener("click", function(){engine_change("duckduckgo");});
 bing.addEventListener("click", function(){engine_change("bing");});
+$(document).ready(load())
+
+// Retrieves settings
+function load() {
+    var engine = "";
+    var keywords = "";
+    chrome.storage.local.get('search', function (result) {
+        engine = result.search;
+        engine_change(engine)
+    });
+}
+
+// Changes the search engine
 function engine_change(engine){
     google.style.display = '';
     duckduckgo.style.display = '';
     bing.style.display = '';
     if (engine == "google"){
+        chrome.storage.local.set({search: "google"});
         web_engine.action = "https://www.google.com/search";
         hover_button.src = "images/google_icon.png";
-        google.style.display = "none";
     }
     if (engine == "duckduckgo"){
+        chrome.storage.local.set({search: "duckduckgo"});
         web_engine.action = "https://www.duckduckgo.com/";
         hover_button.src = "images/duckduckgo.png";
-        duckduckgo.style.display = "none";
     }
     if (engine == "bing"){
+        chrome.storage.local.set({search: "bing"});
         web_engine.action = "https://www.bing.com/search";
         hover_button.src = "images/bing_icon.png";
-        bing.style.display = "none";
     }
     $(".dropdown-content button").css({
         'border-bottom-left-radius': '0px',
