0c0ba22380c3178bf27cfb5b191b74287dc1c428
PopsicleSnow <57603239+PopsicleSnow@users.noreply.github.com>
2020-07-24 11:08:56 -0700

    Colors

--- yes.js
+++ yes.js
@@ -9,17 +9,64 @@ var bg_color = document.getElementById("b_color");
 google.addEventListener("click", function(){engine_change("google");});
 duckduckgo.addEventListener("click", function(){engine_change("duckduckgo");});
 bing.addEventListener("click", function(){engine_change("bing");});
-bg_color.addEventListener("input", function(){background_color(bg_color.value);});
+bg_color.addEventListener("input", function(){theme_color(bg_color.value);});
 $(document).ready(load())
 
-// Retrieves settings and sets them
+// Retrieves settings and loads them
 function load() {
-    var engine = "";
-    var keywords = "";
+
+    // Set search engine to last used
     chrome.storage.local.get('search', function (result) {
         engine = result.search;
-        engine_change(engine)
+        console.log(engine)
+        if (engine != "undefined") {
+            engine_change(engine);
+        }
+        else {engine_change("google");}
     });
+
+    // Sets background color to saved color
+    chrome.storage.local.get('Background_Color', function (result) {
+        color = result.Background_Color;
+        if (color != "undefined") {
+            document.body.style.backgroundColor = result.Background_Color;
+        }
+    });
+
+    // Sets color input to current background color
+    var currentbg_color = getRGB(window.getComputedStyle(document.body).backgroundColor);
+    bg_color.value = rgbToHex(parseInt(currentbg_color["red"]), parseInt(currentbg_color["green"]), parseInt(currentbg_color["blue"]));
+}
+
+// splits rgb color values from strings into an array of strings (must be converted to int by parseInt)
+function getRGB(str){
+    var match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+    return match ? {
+      red: match[1],
+      green: match[2],
+      blue: match[3]
+    } : {};
+}
+
+// function to convert rgb to hex
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+
+// function to convert hex to rgb
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+// Saves settings to chrome local storage
+function saveSettings() {
+    chrome.storage.local.set({Background_Color: bg_color.value});
 }
 
 // Changes the search engine
@@ -46,8 +93,14 @@ function engine_change(engine){
         'border-bottom-right-radius': '20px'});
 }
 
-function background_color(color){
+// changes color of other elements to match theme
+function theme_color(color){
     document.body.style.backgroundColor = color;
+    var rgb_ = hexToRgb(color);
+    rgb_.r += 22;
+    rgb_.g += 22;
+    rgb_.b += 22;
+    document.getElementById("search_box").style.background = `rgb(${rgb_.r}, ${rgb_.g}, ${rgb_.b})`;
 }
 
 // Opens and closes settings
@@ -60,7 +113,7 @@ $(document).click(function(event) {
     // If you click on settings button, open/close
     if ($(event.target).closest("#setting_button").length) {
         if (settings_screen.css('display') == "none") {
-            settings_screen.css('display', 'block');           
+            settings_screen.css('display', 'block');
         }
         else {
             settings_screen.css('display', 'none');
@@ -70,13 +123,14 @@ $(document).click(function(event) {
 
 dragElement(document.getElementById("astronaut"));
 
+// makes elements draggable
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
     if (document.getElementById(elmnt.id + "header")) {
         // if present, the header is where you move the DIV from:
         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } 
+    }
     else {
         // otherwise, move the DIV from anywhere inside the DIV:
         elmnt.onmousedown = dragMouseDown;
